cf6799c463a1746f56160e28d9ecb675aa65d253
Enmanuel <emmanuelperez11@gmail.com>
2024-10-04 09:35:52 -0300

    Backend

--- app/src/main/java/com/bootcamp/conexion_api_news_ejercicio_individual_4_modulo_6/NewsApplication.kt
+++ app/src/main/java/com/bootcamp/conexion_api_news_ejercicio_individual_4_modulo_6/NewsApplication.kt
@@ -1,6 +1,8 @@
 package com.bootcamp.conexion_api_news_ejercicio_individual_4_modulo_6
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class NewsApplication:Application() {
 }
